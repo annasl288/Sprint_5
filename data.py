03c919c51dec27b8236b1a778ed29528a5a45216
Anna Slobodyanyuk <anne.slobodyanyuk@yandex.ru>
2025-02-13 14:50:48 +0300

    Добавлена генерация случайных email и пароля для проверок регистрации

--- data.py
+++ data.py
@@ -1,4 +1,11 @@
+from random import randint
+
 class Person:
     NAME = 'Анна'
     EMAIL = 'annaslobodyanyuk14a111@ya.ru'
     PASSWORD = 'qwerty'
+
+class RandomPerson:
+    NAME = 'Анна_тест'
+    EMAIL = f'annaslobodyanyuk14a{randint(0, 999)}@ya.ru'
+    PASSWORD = str(randint(100000, 999999))
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support import expected_conditions
 
 from locators import RegistrationPage, AuthorizationPage
 from urls import URLS
+from data import RandomPerson
 
 
 class TestRegistration:
@@ -12,9 +13,9 @@ class TestRegistration:
         driver.get(URLS.REGISTRATION_PAGE_URL)
         WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable(RegistrationPage.REGISTRATION_BUTTON))
 
-        driver.find_element(*RegistrationPage.NAME_INPUT).send_keys('Анна')
-        driver.find_element(*RegistrationPage.EMAIL_INPUT).send_keys('annaslobodyanyuk14a120@ya.ru')
-        driver.find_element(*RegistrationPage.PASSWORD_INPUT).send_keys('qwerty')
+        driver.find_element(*RegistrationPage.NAME_INPUT).send_keys(RandomPerson.NAME)
+        driver.find_element(*RegistrationPage.EMAIL_INPUT).send_keys(RandomPerson.EMAIL)
+        driver.find_element(*RegistrationPage.PASSWORD_INPUT).send_keys(RandomPerson.PASSWORD)
 
         driver.find_element(*RegistrationPage.REGISTRATION_BUTTON).click()
         WebDriverWait(driver, 3).until(expected_conditions.presence_of_element_located(AuthorizationPage.HEADER))
@@ -27,8 +28,8 @@ class TestRegistration:
         driver.get(URLS.REGISTRATION_PAGE_URL)
         WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable(RegistrationPage.REGISTRATION_BUTTON))
 
-        driver.find_element(*RegistrationPage.NAME_INPUT).send_keys('Анна')
-        driver.find_element(*RegistrationPage.EMAIL_INPUT).send_keys('annaslobodyanyuk14a120@ya.ru')
+        driver.find_element(*RegistrationPage.NAME_INPUT).send_keys(RandomPerson.NAME)
+        driver.find_element(*RegistrationPage.EMAIL_INPUT).send_keys(RandomPerson.EMAIL)
         driver.find_element(*RegistrationPage.PASSWORD_INPUT).send_keys('123')
 
         driver.find_element(*RegistrationPage.REGISTRATION_BUTTON).click()
